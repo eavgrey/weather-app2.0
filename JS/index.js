cbeaa4e1715c9723a62e63fab6df25b7128a0c1b
eavgrey <81044186+eavgrey@users.noreply.github.com>
2022-09-05 12:46:24 +0300

    Geolocation
    trying to add geolocation mode

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,40 @@ let btn = document.getElementById('btn')
 
 let input = document.getElementById('input')
 
+let switchBtn = document.getElementById('switch')
+let switchBall = document.getElementById('switch-ball') 
 
+
+
+let getWeatherByGeo = () =>{
+  switchBall.style.backgroundColor = ' #3f51b5'
+  switchBtn.style.justifyContent = 'flex-end'
+  switchBall.style.marginRight = ' 7px'
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+      lon = position.coords.longitude;
+      lat = position.coords.latitude;
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
+    `lon=${lon}&appid=${key}`;
+  
+  fetch(url)
+    .then((response)=> response.json())
+    .then( (data) => {
+      console.log(data)
+      output.innerHTML = `
+        <h2 class="city-name">${data.name}</h2>
+        <h4 class="weather">${data.weather[0].main}</h4>
+        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+        <h1 class="temp">${data.main.temp}&#176;</h1>`
+    })
+    .catch(()=>{
+      output.innerHTML = `<h3 class="msg"> City not found</h3>`
+    })
+  })
+}
+}
 
 let getWeather = () => {
   let city = input.value;
@@ -31,6 +64,6 @@ let getWeather = () => {
 
 
 
-
+switchBtn.addEventListener("click", getWeatherByGeo)
 
 btn.addEventListener("click", getWeather)
