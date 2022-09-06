fbc40a56cfa96a25a4d2503b0acacb36029ca23d
eavgrey <81044186+eavgrey@users.noreply.github.com>
2022-09-06 22:51:37 +0300

    Redacting
    New geo button.
    More "readable" code
    Convert K to C

--- JS/index.js
+++ JS/index.js
@@ -1,41 +1,39 @@
-key = "5fd536a4e4ccaaf4de60ad08fb5d3aed";
+"use strict"
 
+let key = "5fd536a4e4ccaaf4de60ad08fb5d3aed";
 let output = document.getElementById("output-container")
 let btn = document.getElementById('btn')
-
 let input = document.getElementById('input')
 
-//slet switchBtn = document.getElementById('switch')
-//let switchBall = document.getElementById('switch-ball') 
-
-
-
-let getWeatherByGeo = () =>{
-  //switchBall.style.backgroundColor = ' #3f51b5'
-  //switchBtn.style.justifyContent = 'flex-end'
- // switchBall.style.marginRight = ' 7px'
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
-      lon = position.coords.longitude;
-      lat = position.coords.latitude;
-
-    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
-    `lon=${lon}&appid=${key}`;
-  
+let getFetch = (url)=>{
   fetch(url)
     .then((response)=> response.json())
     .then( (data) => {
       console.log(data)
-      output.innerHTML = `
+          output.innerHTML = `
         <h2 class="city-name">${data.name}</h2>
         <h4 class="weather">${data.weather[0].main}</h4>
         <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <h1 class="temp">${data.main.temp}&#176;</h1>`
+        <h1 class="temp">${Math.floor (data.main.temp - 273.15) }&#176;</h1>`
     })
     .catch(()=>{
       output.innerHTML = `<h3 class="msg"> City not found</h3>`
     })
+}
+
+
+let getWeatherByGeo = () =>{
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+      let lon = position.coords.longitude;
+      let lat = position.coords.latitude;
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
+    `lon=${lon}&appid=${key}`;
+  getFetch(url)
+  
   })
 }
 }
@@ -44,19 +42,7 @@ let getWeather = () => {
   let city = input.value;
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
   input.value = ''
-  fetch(url)
-    .then((response)=> response.json())
-    .then( (data) => {
-      console.log(data)
-      output.innerHTML = `
-        <h2 class="city-name">${data.name}</h2>
-        <h4 class="weather">${data.weather[0].main}</h4>
-        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <h1 class="temp">${data.main.temp}&#176;</h1>`
-    })
-    .catch(()=>{
-      output.innerHTML = `<h3 class="msg"> City not found</h3>`
-    })
+    getFetch(url)
 }
 
 
@@ -64,6 +50,5 @@ let getWeather = () => {
 
 
 
-//switchBtn.addEventListener("click", getWeatherByGeo)
 
 btn.addEventListener("click", getWeather)
